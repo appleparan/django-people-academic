9bce58bc1cb85f6944a8c8eccc1b6bd9e1f8d022
Jong-su, Kim <appleparan@gmail.com>
2013-09-30 08:29:49 +0900

    fix duplicate bug for multi-lang environment

--- people_academic/cms_plugins.py
+++ people_academic/cms_plugins.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """django-cms plugins for the ``people`` app."""
 from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import get_language
 
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
@@ -15,9 +16,11 @@ class PersonPlugin(CMSPluginBase):
 
     def render(self, context, instance, placeholder):
         #people = Person.people.get_people_list(PersonPluginModel.group)
+        lang = get_language()
         group = instance.group
         people = Person.objects.all() \
             .filter(group__grouptranslation__name__exact=group) \
+            .filter(persontranslation__language=lang) \
             .order_by('ordering')
         context.update({
             'plugin': instance,
